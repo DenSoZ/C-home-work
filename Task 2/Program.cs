4454c85ef9239b3c0968c1d3821b6e7d3564ca88
DenSoZ <denispinka@mail.ru>
2022-09-20 18:44:16 +0300

    add ignore and done task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int a,b;
+Console.Write("Insert first number: ");
+a = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Insert second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+Convert.ToInt32(b);
+if ( a < b )
+{
+    Console.Write("Max = ");
+    Console.Write(b);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.Write(a);
+}
