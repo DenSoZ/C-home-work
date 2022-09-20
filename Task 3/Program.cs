bdc1e13c916accb53ba79b87c1002aa0e80ffd94
DenSoZ <denispinka@mail.ru>
2022-09-20 18:52:13 +0300

    Done task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,21 @@
+﻿int a,b,c,max = 0;
+Console.Write("Insert first number: ");
+a = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Insert second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert third number: ");
+c = Convert.ToInt32(Console.ReadLine());
+max = a;
+if ( max < b )
+{
+   max = b;
+}
+else
+{
+    if ( max < c )
+    {
+        max = c;
+    }
+}
+Console.Write("Max = ");
+Console.Write(max);
