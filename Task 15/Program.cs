617bcef90f74da870c917bc6effc09fa0b5158d0
DenSoZ <denispinka@mail.ru>
2022-09-20 21:51:34 +0300

    Task 15 done

---
+++ Task 15/Program.cs
@@ -0,0 +1,15 @@
+﻿void Holliday(int D)
+{
+    if (D%6 == 0 || D%7 == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.Write("Нет");
+    }
+}
+
+Console.Write("Введите цифру дня недели: ");
+int d = Convert.ToInt32(Console.ReadLine());
+Holliday(d);
