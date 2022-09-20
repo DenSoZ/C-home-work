9c34772cd4f124122a053968c293495f1afecc92
DenSoZ <denispinka@mail.ru>
2022-09-20 18:58:25 +0300

    done 4 task

---
+++ Task 4/Program.cs
@@ -0,0 +1,12 @@
+﻿int a;
+Console.Write("Insert number: ");
+a = Convert.ToInt32 (Console.ReadLine());
+
+if (a%2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
