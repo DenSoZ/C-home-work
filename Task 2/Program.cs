99e53278a9a768da20619ac3461719dc38e5ea5d
DenSoZ <denispinka@mail.ru>
2022-09-20 18:50:43 +0300

    fix some code

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -4,7 +4,6 @@ Console.Write("Insert first number: ");
 a = Convert.ToInt32 (Console.ReadLine());
 Console.Write("Insert second number: ");
 b = Convert.ToInt32(Console.ReadLine());
-Convert.ToInt32(b);
 if ( a < b )
 {
     Console.Write("Max = ");
