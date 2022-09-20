1d5dc217d95756b814f0ee7f7308a1381ef61b4f
DenSoZ <denispinka@mail.ru>
2022-09-20 21:16:56 +0300

    task 10 done

---
+++ Task 10/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] IntToArray(int N)
+{
+    int c = N;
+    int count = 0;
+    while (c > 0)
+    {
+        c = c/10;
+        count ++;
+
+    }
+
+    int[] array = new int[count];
+    for (int i = 1; i <= count; i++)
+    {
+        array[count-i] = N % 10;
+        N = N /10;
+    }
+    return array;
+}
+
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] arr = IntToArray(a);
+Console.WriteLine(string.Join(" ", arr));
+Console.WriteLine("Второй элемент массива = " + arr[1]);
