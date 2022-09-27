b15b7a4f9613713ffa9d87913f4d657ce9d24448
DenSoZ <denispinka@mail.ru>
2022-09-27 19:46:14 +0300

    Task 25 done

---
+++ Task 25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Число А в степени B = {Math.Pow(A,B)}");
