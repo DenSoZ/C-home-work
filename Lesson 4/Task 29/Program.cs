9f8ed77c69c5e937d5dd4c12d2ef567ee9e42b91
DenSoZ <denispinka@mail.ru>
2022-09-27 20:57:38 +0300

    change directory

---
+++ Lesson 4/Task 29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int[] arr = new int[8];
+Random rnd = new Random();
+for (int i = 0; i < 8; i++)
+{
+    arr[i] = rnd.Next(1,100);
+}
+
+
+Console.WriteLine("[" + string.Join(", ", arr)+ "]");
