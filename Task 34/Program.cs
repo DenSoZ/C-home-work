941cae856c8ce68564f9471328c2f67865cd2fb5
DenSoZ <denispinka@mail.ru>
2022-09-27 20:16:47 +0300

    Lesson 5 task 34 done

---
+++ Task 34/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите длину массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+Random rnd = new Random();
+for (int i = 0; i < N; i++)
+{
+    arr[i] = rnd.Next(100,999);
+}
+Console.WriteLine("[" + string.Join(", ", arr)+ "]");
+int count = 0;
+for (int i = 0; i < N; i++)
+{
+    if (arr[i]%2 == 0)
+    {
+        count++;
+    }
+}
+Console.Write($"Количество четных элементов массива = {count}");
