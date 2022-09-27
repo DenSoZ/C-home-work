0f52f72d803cccbb2fd64fdc4d8fd2121c6738bb
DenSoZ <denispinka@mail.ru>
2022-09-27 19:21:39 +0300

    Task 19 done

---
+++ Task 19/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+int count = 5;
+int[] IntToArray(int N)
+{
+
+    int[] array = new int[count];
+    for (int i = 1; i <= count; i++)
+    {
+        array[count-i] = N % 10;
+        N = N /10;
+    }
+    return array;
+}
+
+int check = 0;
+Console.Write("Введите пятизначное число: ");
+int p = Convert.ToInt32(Console.ReadLine());
+int[] arr = IntToArray(p);
+
+for (int i = 0; i < 2; i++)
+{
+    if (arr[i] == arr[4-i])
+    {
+        check++;
+    }
+}
+if (check == 2)
+{
+    Console.WriteLine("Это палиндром");
+}
+else
+{
+    Console.WriteLine("Это не палиндром");
+}
