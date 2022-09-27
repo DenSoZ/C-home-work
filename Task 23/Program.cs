e87ab3aa484c500c493d11146b386f3348e619b3
DenSoZ <denispinka@mail.ru>
2022-09-27 19:40:11 +0300

    Task 23 done

---
+++ Task 23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < N+1; i++)
+{
+    Console.Write($"{Math.Pow(i,2)}");
+    if (i < N)
+    {
+        Console.Write(", ");
+    }
+}
