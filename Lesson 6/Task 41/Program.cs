7514ed9a12a07f63045eb3425ffa89396ec498b7
DenSoZ <denispinka@mail.ru>
2022-10-07 12:46:51 +0300

    Task 41 done

---
+++ Lesson 6/Task 41/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите длину массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+int count = 0;
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"Введите {i+1} элемент: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.Write($"Элементов больше нуля: {count}");
