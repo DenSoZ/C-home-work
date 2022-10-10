c0a20fc9024ef0d99b804460d0cd0088e0282be3
DenSoZ <denispinka@mail.ru>
2022-10-10 14:01:20 +0300

    Task 47 done

---
+++ Lesson 7/Task 47/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] GenerateRandomArray (double[,] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+        {
+          for (int j = 0; j < n; j++)
+            {
+                array[i,j] = Math.Round(rnd.NextDouble() * 20 - 10, 2);
+            }
+        }
+    return array;
+}
+
+
+double[,] randomArray = new double[m, n];
+GenerateRandomArray(randomArray);
+for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+          Console.Write(randomArray[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
