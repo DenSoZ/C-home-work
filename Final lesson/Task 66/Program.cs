7fb5e3638ecab4e5eecdb7cbf74aa40fb6200ff1
DenSoZ <denispinka@mail.ru>
2022-10-17 13:11:28 +0300

    Task 66 done

---
+++ Final lesson/Task 66/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N = int.Parse(Console.ReadLine());
+
+void SwapNumbers()
+{
+    int s = new int();
+    s = M;
+    M = N;
+    N = s;
+}
+
+int Sum = 0;
+
+if(M > N) SwapNumbers();
+
+for (int i = M; i < N+1; i++)
+{
+    Sum += i;
+}
+
+Console.Write(Sum);
