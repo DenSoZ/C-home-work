60a7303ad5bb13f4b37e0b3caded720c1e2af168
DenSoZ <denispinka@mail.ru>
2022-10-17 12:57:06 +0300

    Task 64 done

---
+++ Final lesson/Task 64/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+void NaturalNumbersList(int n)
+{
+    if(n > 0)
+    {
+        Console.Write(n + " ");
+        NaturalNumbersList(n-1);
+    }
+}
+
+
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+NaturalNumbersList(N);
